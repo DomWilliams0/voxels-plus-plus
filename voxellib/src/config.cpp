ffd646646b1733f4da3af9f1b1de95174d7eab32
Dom Williams <me@domwillia.ms>
2019-05-20 19:04:56 +0100

    Add env var config overriding

--- voxellib/src/config.cpp
+++ voxellib/src/config.cpp
@@ -1,6 +1,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/thread/thread_pool.hpp>
+#include <boost/lexical_cast.hpp>
 #include "config.h"
 #include "world/generation/generator.h"
 
@@ -33,8 +34,26 @@ namespace config {
         out.append("/config.xml");
     }
 
+    template<typename T>
+    static T get(const boost::property_tree::ptree &tree, const std::string &key) {
+        // check env first
+        const char *prefix = "VOX_";
+        std::string env_key(prefix);
+        env_key.append(key);
+
+        for (auto &c : env_key)
+            c = std::toupper(c == '.' ? '_' : c);
+
+        char *env_value = std::getenv(env_key.c_str());
+        if (env_value)
+            return boost::lexical_cast<T>(env_value);
+
+        // get from config
+        return tree.get<T>(key);
+    }
+
     static int thread_count(const boost::property_tree::ptree &tree, const char *key) {
-        unsigned int count = tree.get<int>(key, 0);
+        unsigned int count = get<int>(tree, key);
         if (count <= 0)
             count = boost::thread::hardware_concurrency() * 2;
 
@@ -45,7 +64,7 @@ namespace config {
     }
 
     static GeneratorType generator(const boost::property_tree::ptree &tree, std::string &out) {
-        std::string str = tree.get<std::string>("terrain.generator");
+        std::string str = get<std::string>(tree, "terrain.generator");
         GeneratorType type;
 
         if (str == "flat")
